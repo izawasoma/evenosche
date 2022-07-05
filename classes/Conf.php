0536821a57048f09ef6224ca3414181d9c4db949
izawasoma <izawasoma@gmail.com>
2022-07-05 12:28:45 +0900

    confをignoreから外した

---
+++ classes/Conf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LocalMyStudy\Evenosche\Classes;
+
+/* class Conf{
+    const DB_DNS = "mysql:host=localhost;dbname=evenosche;charset=utf8";
+    const DB_USERNAME = "root";
+    const DB_PASSWORD = "";
+} */
+
+class Conf{
+    const DB_DNS = "mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_ce2395704d75e21;charset=utf8";
+    const DB_USERNAME = "b3e777a16edc89";
+    const DB_PASSWORD = "0f2d4b13";
+}
+?>
